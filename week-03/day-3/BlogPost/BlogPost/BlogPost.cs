1f2dbf3bca75690b24f74b5a4965da841d1fa546
Stepan Papez <stepan.papez@gmail.com>
2019-09-25 13:44:21 +0200

    modify class file

--- week-03/day-3/BlogPost/BlogPost/BlogPost.cs
+++ week-03/day-3/BlogPost/BlogPost/BlogPost.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace BlogPost
 {
-    class Class1
+    public class BlogPost
     {
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string PublicationDate { get; set; }
+
+        public BlogPost(string authorName, string title, string text, string publicationDate)
+        {
+            this.AuthorName = authorName;
+            this.Title = title;
+            this.Text = text;
+            this.PublicationDate = publicationDate;
+        }
     }
 }
