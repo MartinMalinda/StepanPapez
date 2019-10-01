f434ff9dee2654ba921f98d3bbcd956c20152f5b
Stepan Papez <stepan.papez@gmail.com>
2019-10-01 15:26:27 +0200

    (W04D02) add GreenFoxInheritance practice

---
+++ week-04/day-2/GreenFoxInheritance/GreenFoxInheritance/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFoxInheritance
+{
+    public enum Gender
+    {
+        male,
+        female,
+    }
+}
---
+++ week-04/day-2/GreenFoxInheritance/GreenFoxInheritance/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFoxInheritance
+{
+    class Person
+    {
+        private string name = "Jane Doe";
+        private int age = 30;
+        public Gender gender = Gender.female;
+
+        public Person()
+        {
+        }
+
+        public Person(string name , int age, Gender gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {name}, a {age} years old {gender}.");
+        }
+        public void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+    }
+}
