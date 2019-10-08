7426c73df6ef21dfaad79ad4248a00aa36607ad3
Stepan Papez <stepan.papez@gmail.com>
2019-10-08 17:26:28 +0200

    add PetrolStation exercise

---
+++ week-03/day-4/PetrolStation/PetrolStation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PetrolStation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var station = new Station(100);
+            var car = new Car();
+
+            station.Refill(car);
+
+            Console.WriteLine(station.GasAmount);
+            Console.WriteLine(car.GasAmount);
+        }
+    }
+}
---
+++ week-03/day-4/PetrolStation/PetrolStation/Station.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetrolStation
+{
+    // GasAmount
+    // Refill(car) -> decreases the gasAmount by the capacity of the car and increases the cars gasAmount
+    class Station
+    {
+        public int GasAmount { get; set; }
+        
+        public Station(int gasAmount)
+        {
+            this.GasAmount = gasAmount;
+        }
+
+        public void Refill(Car car)
+        {
+            this.GasAmount -= car.Capacity;
+            car.GasAmount += car.Capacity;
+        }
+    }
+}
