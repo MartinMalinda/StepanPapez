ef6d79048e7fc9720ea89c93e31d483fb02f2060
Stepan Papez <stepan.papez@gmail.com>
2019-10-22 16:57:58 +0200

    Add - ASP.NET - W6 - D2

---
+++ week-06/day-2/ASP.NETApp/Controllers/RestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETApp.Controllers
+{
+    [Route("api")]
+    public class RestController : Controller
+    {
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
+
+        [HttpGet("greeting")]
+        public Greeting Greet()
+        {
+           
+        }
+    }
+}
