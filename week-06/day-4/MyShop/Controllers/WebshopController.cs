fcf5cf56e87012a8e93f307850a8066f680d832e
Stepan Papez <stepan.papez@gmail.com>
2019-10-31 15:15:06 +0100

    Add - MyShop project - W7 - D4

---
+++ week-06/day-4/MyShop/Controllers/WebshopController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+using MyShop.Service;
+
+namespace MyShop.Controllers
+{
+    [Route("webshop")]
+    public class WebshopController : Controller
+    {
+        
+        [HttpGet("")]
+        public IActionResult GetAll()
+        {
+            return View(WebshopService.ShopItemsList);
+        }
+
+        [HttpGet("only-available")]
+        public IActionResult OnlyAvailable()
+        {
+            return View("GetAll", WebshopService.GetOnlyAvailable(WebshopService.ShopItemsList));
+        }
+    }
+}
---
+++ week-06/day-4/MyShop/Models/ShopItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShop.Models
+{
+    public class ShopItem
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+
+
+        public ShopItem(string name, string description, double price, int quantity)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
