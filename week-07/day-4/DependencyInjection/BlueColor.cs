934b3ad1b45cee1c62f8bcc6de4b4bc9c4698908
Stepan Papez <stepan.papez@gmail.com>
2019-10-31 14:08:07 +0100

    Add - DependencyInjection - W7 - D4

---
+++ week-07/day-4/DependencyInjection/BlueColor.cs
@@ -0,0 +1,16 @@
+﻿using DependencyInjection.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class BlueColor : IColor
+    {
+        public void PrintColor()
+        {
+            Console.WriteLine("It is blue in color...");
+        }
+    }
+}
