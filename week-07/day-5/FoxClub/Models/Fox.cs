caa5e94300a23bb3d64cfbb6825523371b9c538a
Stepan Papez <stepan.papez@gmail.com>
2019-11-01 15:55:54 +0100

    Add - Fox model - W7 - D5

---
+++ week-07/day-5/FoxClub/Models/Fox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxClub.Models
+{
+    public class Fox
+    {
+        public string Name { get; set; }
+        private List<string> listOfTricks;
+        private string food;
+        private string drink;
+    }
+}
